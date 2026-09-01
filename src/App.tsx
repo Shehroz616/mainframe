d940c404442e2d40ef944181d3c47be7abe9d50f
Shehroz <shehrozsaleem616@gmail.com>
2026-09-01 15:03:47 +0500

    remove extra space

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,6 @@ import Marquee from './components/Marquee';
 import {Hero} from './components/Hero';
 import {Navbar} from './components/Navbar';
 
-
-
-
-
-
-
-
-
 function App() {
   return (
     <main className="page-shell relative min-h-screen">
