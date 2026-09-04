1e97a1e403bb752ddea3cddb9766f718cad6a413
Shehroz <shehrozsaleem616@gmail.com>
2026-09-04 15:11:24 +0500

    refactor: remove AfterTestimonials component and adjust App layout

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Marquee from './components/Marquee';
 import {Hero} from './components/Hero';
 import {Navbar} from './components/Navbar';
 import Testimonials from './components/Testimonials';
-import AfterTestimonials from './components/AfterTestimonials';
 import ContactForm from './components/ContactForm';
 import Footer from './components/Footer';
 import AdvancedDentistry from './components/AdvancedDentistry';
@@ -35,11 +34,10 @@ function App() {
     <main id="top" className="page-shell relative min-h-screen">
       <Navbar />
       <Hero />
-      <AdvancedDentistry />
       <Marquee />
+      <AdvancedDentistry />
       <Services />
       <Testimonials />
-      <AfterTestimonials />
       <ContactForm />
       <Footer />
     </main>
